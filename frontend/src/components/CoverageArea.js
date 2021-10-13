f20272db0c6fca5fedb9d9eb919b1242f3358efb
acargorkem <acargorkem@outlook.com>
2021-10-13 17:25:36 +0300

    Seperate coverage area logic from UI component

--- frontend/src/components/CoverageArea.js
+++ frontend/src/components/CoverageArea.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Entity, PolygonGraphics } from 'resium';
-import { Math as CesiumMath, Cartesian3, Color } from 'cesium';
-
-const radiusOfEarthInMeters = 6371008.7714;
+import { Color } from 'cesium';
+import { getCoverageAreaBoundsForFeatures } from '../services/coverageArea';
 
 class CoverageArea extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       coverageAreaBounds: [],
+      coverageDistanceForMinus65Db: 250,
     };
   }
 
@@ -18,80 +18,17 @@ class CoverageArea extends React.Component {
   componentDidUpdate(prevProps) {
     if (prevProps.geospatialData !== this.props.geospatialData) {
       const features = [...this.props.geospatialData.geoJson.features];
-      this.addPolygonGraphicsAroundFeatures(features);
+      this.setCoverageAreaBounds(features);
     }
   }
 
-  addPolygonGraphicsAroundFeatures(features) {
-    let positions = [];
-    features.forEach((feature) => {
-      const lon = feature.geometry.coordinates[0];
-      const lat = feature.geometry.coordinates[1];
-      const height = feature.geometry.coordinates[2];
-      let coverageAreaCenterPoints = this.getCoverageAreaCenterPoints(
-        lon,
-        lat,
-        height,
-        250
-      );
+  setCoverageAreaBounds(features) {
+    let distance = this.state.coverageDistanceForMinus65Db;
+    let positions = getCoverageAreaBoundsForFeatures(features, distance);
 
-      coverageAreaCenterPoints.forEach((point) => {
-        let position = this.getHexagonBounds(point[0], point[1], point[2], 250);
-        positions.push(position);
-      });
-    });
     this.setState({
       coverageAreaBounds: [...positions],
     });
-  }
-
-  getCoverageAreaCenterPoints(lonDegree, latDegree, height, radius) {
-    let centerPositions = [];
-    let d = radius / radiusOfEarthInMeters;
-    let latRadian = CesiumMath.toRadians(latDegree);
-    let lonRadian = CesiumMath.toRadians(lonDegree);
-    for (let i = 0; i < 360; i += 120) {
-      let radians = CesiumMath.toRadians(i);
-      var newLatitudeInRadian = Math.asin(
-        Math.sin(latRadian) * Math.cos(d) +
-          Math.cos(latRadian) * Math.sin(d) * Math.cos(radians)
-      );
-      var newLongitudeInRadian =
-        lonRadian +
-        Math.atan2(
-          Math.sin(radians) * Math.sin(d) * Math.cos(latRadian),
-          Math.cos(d) - Math.sin(latRadian) * Math.sin(newLatitudeInRadian)
-        );
-      let position = [newLongitudeInRadian, newLatitudeInRadian, height];
-      centerPositions.push(position);
-    }
-    return centerPositions;
-  }
-
-  getHexagonBounds(lonRadian, latRadian, height, radius) {
-    let bounds = [];
-    let d = radius / radiusOfEarthInMeters;
-    for (let i = 0; i < 360; i += 60) {
-      let radians = CesiumMath.toRadians(i);
-      var newLatitudeInRadian = Math.asin(
-        Math.sin(latRadian) * Math.cos(d) +
-          Math.cos(latRadian) * Math.sin(d) * Math.cos(radians)
-      );
-      var newLongitudeInRadian =
-        lonRadian +
-        Math.atan2(
-          Math.sin(radians) * Math.sin(d) * Math.cos(latRadian),
-          Math.cos(d) - Math.sin(latRadian) * Math.sin(newLatitudeInRadian)
-        );
-
-      let position = Cartesian3.fromRadians(
-        newLongitudeInRadian,
-        newLatitudeInRadian,
-        height
-      );
-      bounds.push(position);
-    }
-    return bounds;
   }
 
   render() {
---
+++ frontend/src/services/coverageArea.js
@@ -0,0 +1,78 @@
+import { Math as CesiumMath, Cartesian3 } from 'cesium';
+
+const radiusOfEarthInMeters = 6371008.7714;
+
+const getCoverageAreaCenterPoints = (lonDegree, latDegree, height, radius) => {
+  let centerPositions = [];
+  let d = radius / radiusOfEarthInMeters;
+  let latRadian = CesiumMath.toRadians(latDegree);
+  let lonRadian = CesiumMath.toRadians(lonDegree);
+  for (let i = 0; i < 360; i += 120) {
+    let radians = CesiumMath.toRadians(i);
+    var newLatitudeInRadian = Math.asin(
+      Math.sin(latRadian) * Math.cos(d) +
+        Math.cos(latRadian) * Math.sin(d) * Math.cos(radians)
+    );
+    var newLongitudeInRadian =
+      lonRadian +
+      Math.atan2(
+        Math.sin(radians) * Math.sin(d) * Math.cos(latRadian),
+        Math.cos(d) - Math.sin(latRadian) * Math.sin(newLatitudeInRadian)
+      );
+    let position = [newLongitudeInRadian, newLatitudeInRadian, height];
+    centerPositions.push(position);
+  }
+  return centerPositions;
+};
+
+const getHexagonBoundsInCartesian3 = (lonRadian, latRadian, height, radius) => {
+  let bounds = [];
+  let d = radius / radiusOfEarthInMeters;
+  for (let i = 0; i < 360; i += 60) {
+    let radians = CesiumMath.toRadians(i);
+    var newLatitudeInRadian = Math.asin(
+      Math.sin(latRadian) * Math.cos(d) +
+        Math.cos(latRadian) * Math.sin(d) * Math.cos(radians)
+    );
+    var newLongitudeInRadian =
+      lonRadian +
+      Math.atan2(
+        Math.sin(radians) * Math.sin(d) * Math.cos(latRadian),
+        Math.cos(d) - Math.sin(latRadian) * Math.sin(newLatitudeInRadian)
+      );
+
+    let position = Cartesian3.fromRadians(
+      newLongitudeInRadian,
+      newLatitudeInRadian,
+      height
+    );
+    bounds.push(position);
+  }
+  return bounds;
+};
+
+export const getCoverageAreaBoundsForFeatures = (features, distance) => {
+  let positions = [];
+  features.forEach((feature) => {
+    const lon = feature.geometry.coordinates[0];
+    const lat = feature.geometry.coordinates[1];
+    const height = feature.geometry.coordinates[2];
+    let coverageAreaCenterPoints = getCoverageAreaCenterPoints(
+      lon,
+      lat,
+      height,
+      distance
+    );
+
+    coverageAreaCenterPoints.forEach((point) => {
+      let position = getHexagonBoundsInCartesian3(
+        point[0],
+        point[1],
+        point[2],
+        distance
+      );
+      positions.push(position);
+    });
+  });
+  return positions;
+};
