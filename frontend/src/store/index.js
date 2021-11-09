87eb123549a0fe319e9347250e00b89316479ae3
acargorkem <acargorkem@outlook.com>
2021-11-09 15:45:50 +0300

    Create infobox reducer and add to redux store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import geojsonSliceReducer from './geojsonSlice';
 import markerSliceReducer from './markerSlice';
+import infoboxSliceReducer from './infoboxSlice';
 
 export const store = configureStore({
   reducer: {
     coverageArea: geojsonSliceReducer,
     markers: markerSliceReducer,
+    infobox: infoboxSliceReducer,
   },
 });
 
