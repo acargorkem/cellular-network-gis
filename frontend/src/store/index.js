d9757b8eae74a19d987f8f4b9a28e5be12a159ed
acargorkem <acargorkem@outlook.com>
2021-11-11 15:37:19 +0300

    Add coverageModelControl to redux store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import geojsonSliceReducer from './geojsonSlice';
 import markerSliceReducer from './markerSlice';
 import infoboxSliceReducer from './infoboxSlice';
+import coverageControlReducer from './coverageControlSlice';
 
 export const store = configureStore({
   reducer: {
     coverageArea: geojsonSliceReducer,
     markers: markerSliceReducer,
     infobox: infoboxSliceReducer,
+    coverageControl: coverageControlReducer,
   },
 });
 
