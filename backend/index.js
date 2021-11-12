0088bef556be795224d1d869da3ae2514a0d38d1
acargorkem <acargorkem@outlook.com>
2021-11-12 17:17:40 +0300

    Implement use multer memorystorage on file upload

--- backend/index.js
+++ backend/index.js
@@ -3,10 +3,9 @@ const helmet = require('helmet');
 const cors = require('cors');
 const multer = require('multer');
 
-const fs = require('fs');
 const toGeoJson = require('@tmcw/togeojson');
 const { DOMParser } = require('xmldom');
-const decompress = require('decompress');
+const AdmZip = require('adm-zip');
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -19,19 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 
 const allowedExtensions = /(\.kmz|\.kml)$/i;
 
-const getRandomInt = (max) => Math.floor(Math.random() * max);
-
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, 'uploads');
-  },
-  filename: (req, file, callback) => {
-    const unixTimeStamp = Date.now();
-    const randomNumber = getRandomInt(9999);
-    const extension = file.originalname.slice(-3);
-    callback(null, `${unixTimeStamp}_${randomNumber}.${extension}`);
-  },
-});
+const storage = multer.memoryStorage();
 
 const upload = multer({
   storage,
@@ -43,18 +30,14 @@ const upload = multer({
   },
 }).single('file');
 
-const unzip = async (name) => {
+const unzip = (zipFileBuffer) => {
   try {
-    const files = await decompress(`uploads/${name}`, 'uploads', {
-      map: (file) => {
-        const fileName = name.slice(0, -4);
-        file.path = fileName + file.path.slice(-4);
-        return file;
-      },
-    });
-    return files;
-  } catch (err) {
-    return console.error(err);
+    const zip = new AdmZip(zipFileBuffer);
+    const entries = zip.getEntries();
+    const kmlEntry = entries.find((entry) => entry.entryName.endsWith('.kml'));
+    return kmlEntry.getData().toString('utf-8');
+  } catch (e) {
+    return null;
   }
 };
 
@@ -67,34 +50,36 @@ app.post('/file-upload', async (req, res) => {
       return res.status(400).send({ errorMessage: err.toString() });
     }
     const { file } = req;
-
     if (!file) {
       const error = new Error('Please upload a file');
       return res.status(400).send({ errorMessage: error.toString() });
     }
-    let filePath = file.filename;
 
+    let fileString;
     if (file.originalname.endsWith('.kmz')) {
-      const unzippedFile = await unzip(file.buffer);
-      if (unzippedFile.length === 0) {
+      fileString = unzip(file.buffer);
+      if (!fileString) {
         return res.status(400).send({
           errorMessage: 'Not valid kmz file!',
         });
       }
-      filePath = unzippedFile[0].path;
+    } else {
+      fileString = file.buffer.toString('utf-8');
+      if (!fileString) {
+        return res.status(400).send({
+          errorMessage: 'Not valid kml file!',
+        });
+      }
     }
-
-    const kml = new DOMParser().parseFromString(
-      fs.readFileSync(`uploads/${filePath}`, 'utf8')
-    );
+    const kml = new DOMParser().parseFromString(fileString);
     const geoJson = toGeoJson.kml(kml);
-
     if (geoJson.features.length === 0) {
       return res.status(400).send({
         errorMessage:
           'No features found in the file. Please make sure the file contains feature collection.',
       });
     }
+    delete file.buffer;
     return res.send({ file, geoJson });
   });
 });
